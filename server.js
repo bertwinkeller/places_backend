d8475aa6ef7bdbb27bea44682e9a901bb0790551
Bert Keller <bertwinkeller@gmail.com>
2020-04-11 10:45:05 -0700

    put env in git ignore

--- server.js
+++ server.js
@@ -6,10 +6,12 @@ const placesRoutes = require('./routes/places-routes')
 const userRoutes = require('./routes/users-routes')
 const app = express()
 
-app.use(bodyParser.json())
+const API_KEY = process.env.API_KEY
 
+app.use(bodyParser.json())
 app.use('/api/places', placesRoutes)
 app.use('/api/users', userRoutes)
 
 app.listen(5000)
 
+console.log(API_KEY)
