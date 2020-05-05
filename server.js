96a9d3b846da1936f421dbb98efe74d5f52c6548
Bert Keller <bertwinkeller@gmail.com>
2020-05-05 13:19:03 -0700

    new db

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.use('/api/places', placesRoutes)
 app.use('/api/users', userRoutes)
 
 mongoose
-.connect('mongodb+srv://bertwinkeller:Daintydeethegolden15!@placesdb-c9vha.mongodb.net/placesDB?retryWrites=true&w=majority', {
+.connect('mongodb+srv://bertwinkeller:Daintydeethegolden15!@placesdb-c9vha.mongodb.net/places?retryWrites=true&w=majority', {
     useCreateIndex: true,
     useFindAndModify: true,
     useNewUrlParser: true,
